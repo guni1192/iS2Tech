6ac90577e3504bbe7c54a3a0c037f35e3aa1301f
guni973 <ad2314ce71926@gmail.com>
2017-08-11 17:15:25 +0900

    create article post form

--- article/urls.py
+++ article/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from .views import IndexView
+from .views import IndexView, PostArticleView
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='index')
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^post$', PostArticleView.as_view(),
+        name='post_article'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 from .models import Article
 
+
 class IndexView(ListView):
-    template_name = 'article/index.html'
     model = Article
+    template_name = 'article/index.html'
+
+
+class PostArticleView(CreateView):
+    model = Article
+    fields = ('title', 'detail')
+    template_name = 'article/post_article.html'
+    success_url = '/article/post'
