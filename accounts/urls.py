0311cdac2fbecd8dbf4ccd58158f89dedd3914de
guni973 <ad2314ce71926@gmail.com>
2017-08-12 23:40:34 +0900

    add decorator about login_required

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login,logout
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
     url(r'^login/$', login,
--- article/views.py
+++ article/views.py
@@ -1,26 +1,32 @@
 from django.shortcuts import render, reverse
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from .models import Article
 from .forms import ArticleModelForm
 
 
+@method_decorator(login_required, name='dispatch')
 class IndexView(ListView):
     model = Article
     template_name = 'article/index.html'
 
 
+@method_decorator(login_required, name='dispatch')
 class PostArticleView(CreateView):
     form_class = ArticleModelForm
     template_name = 'article/post_article.html'
     success_url = '/article/post'
 
 
+@method_decorator(login_required, name='dispatch')
 class ArticleDetailView(DetailView):
     model = Article
     template_name = 'article/detail.html'
 
 
+@method_decorator(login_required, name='dispatch')
 class UpdateArticleView(UpdateView):
     form_class = ArticleModelForm
     template_name = 'article/post_article.html'
@@ -29,6 +35,7 @@ class UpdateArticleView(UpdateView):
         return reverse('article:update', args=(self.object.id,))
 
 
+@method_decorator(login_required, name='dispatch')
 class DeleteArticleView(DeleteView):
     model = Article
     template_name = 'article/article_confirm_delete.html'
