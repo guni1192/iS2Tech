a2d57e5ab0fb473540647626989e2684c0c52efb
guni973 <ad2314ce71926@gmail.com>
2017-08-17 02:46:41 +0900

    create user mypage, and user info update page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,32 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import login, logout, password_change, password_change_done
+from django.urls import reverse_lazy
+from .views import CreateAccountView, UpdateAccountView, UserMyPageView
 
 urlpatterns = [
     url(r'^login/$', login,
         {'template_name': 'accounts/login.html'}, name='login'),
-    url(r'^logout/$', logout, name='logout')
+    url(r'^logout/$', logout, name='logout'),
+    url(r'^create/user/$',
+        CreateAccountView.as_view(), name='create'),
+    url(r'^(?P<pk>[0-9]+)/update/$',
+        UpdateAccountView.as_view(), name='update'),
+    url(r'^password/change/$',
+        password_change,
+        {
+            'post_change_redirect': reverse_lazy('accounts:pwd_change_done'),
+            'template_name': 'accounts/password_change_form.html',
+        },
+        name='pwd_change'
+        ),
+
+    url(r'^password/changeed/$',
+        password_change_done,
+        {
+            'template_name': 'accounts/password_change_done.html',
+        },
+        name='pwd_change_done'
+        ),
+    url(r'^(?P<pk>[0-9]+)/mypage/$',
+        UserMyPageView.as_view(), name='mypage'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import reverse
+from django.views.generic import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-# Create your views here.
+
+class CreateAccountView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = 'accounts/user_edit.html'
+
+    def get_success_url(self):
+        return reverse('index')
+
+
+class UpdateAccountView(UpdateView):
+    model = User
+    form_class = UserChangeForm
+    template_name = 'accounts/user_edit.html'
+
+    def get_success_url(self):
+        return reverse('accounts:edit-user', args=(self.object.id, ))
+
+
+class UserMyPageView(DetailView):
+    model = User
+    template_name = 'accounts/user_page_top.html'
